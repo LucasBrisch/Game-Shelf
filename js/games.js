7d0393a6223c83b3febd658367cc20251477fddf
Lucas Brisch <lucasbrisch@gmail.com>
2025-08-01 15:57:04 -0300

    removed files

--- js/games.js
+++
@@ -1,6 +0,0 @@
-let indexAtual = <?php echo $indexAtual; ?>;
-function passarPagina(direcao) {
-    indexAtual += direcao;
-    if (indexAtual < 0) indexAtual = 0;
-    window.location.href = `games.php?offset=${indexAtual}`;
-}
